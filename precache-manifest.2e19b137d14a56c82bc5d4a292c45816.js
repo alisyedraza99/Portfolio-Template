a566378b8a16e223f7ab164d4b54b6ef1d5b2f98
Ali Syed Raza <alisyedraza99@gmail.com>
2019-01-05 00:09:08 -0500

    Updates

---
+++ precache-manifest.2e19b137d14a56c82bc5d4a292c45816.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "5df32c1880220d820e61411d5ce10345",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/PortfolioTemplate.5df32c18.png"
+  },
+  {
+    "revision": "071e76bd6120d369e6a6",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/css/main.e0f31ae5.chunk.css"
+  },
+  {
+    "revision": "0485c5cd8e83de69f172",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/js/1.0485c5cd.chunk.js"
+  },
+  {
+    "revision": "daedd0b8a97d0ded3a49",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/js/runtime~main.daedd0b8.js"
+  },
+  {
+    "revision": "21a8d6557a022ce9c214f6dd22b5d870",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/Ali_Avatar_V1.21a8d655.svg"
+  },
+  {
+    "revision": "b40322412557793a7f063b8a2f060084",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/AnalogClock.b4032241.png"
+  },
+  {
+    "revision": "b454bdb23495357702cc48f8805e03fa",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/DrumKit.b454bdb2.png"
+  },
+  {
+    "revision": "4260b23e4e435c5c567e2c753acf550b",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/JavaPaint.4260b23e.png"
+  },
+  {
+    "revision": "6857b6657ec4bb71738c67892dcc54a6",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/LastIsland.6857b665.png"
+  },
+  {
+    "revision": "67ea02156e3c3d576e2ea862cf1c2bcf",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/MoleGenVR.67ea0215.png"
+  },
+  {
+    "revision": "7f6dd0cfbe6cefa62ef31b6075024168",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/PortfolioSPA.7f6dd0cf.png"
+  },
+  {
+    "revision": "071e76bd6120d369e6a6",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/js/main.071e76bd.chunk.js"
+  },
+  {
+    "revision": "8234cab5a4f28f8c5fb4d88eece1edce",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/ResumeTemplate.8234cab5.png"
+  },
+  {
+    "revision": "746b0a4b32a0b41f417da4362e4ac73d",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/ScareEmOff.746b0a4b.png"
+  },
+  {
+    "revision": "6a04b3cdecd90d7f0753d39cc999fece",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/SpeechRecognition.6a04b3cd.png"
+  },
+  {
+    "revision": "d61030d1524fb74e30b7789f750af581",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/TypingSpeedTester.d61030d1.png"
+  },
+  {
+    "revision": "35e407b9a36b6e443d103c3d78726600",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/WebPaint.35e407b9.png"
+  },
+  {
+    "revision": "adfb06f6dc29d08dafd578e57b034274",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/Ali_Avatar_Joker.adfb06f6.svg"
+  },
+  {
+    "revision": "a11727822738002f85931308f703a731",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/Ali_Avatar_Kratos.a1172782.svg"
+  },
+  {
+    "revision": "43599252597af8c326bb9650f7d8a2d9",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/Ali_Avatar_Soccer.43599252.svg"
+  },
+  {
+    "revision": "8ccb4ec4225b290726ae9be975220ff4",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/static/media/sample.8ccb4ec4.jpg"
+  },
+  {
+    "revision": "7df15cd323e21b44eb5a4c5e18a29a10",
+    "url": "/alisyedraza99/Ali_Portfolio_SPA/index.html"
+  }
+];
